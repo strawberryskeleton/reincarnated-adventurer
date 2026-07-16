91bccf32c5311f69e0c33879ba6f176d5e42001b
strawberryskeleton <xradhi5685@gmail.com>
2026-07-16 20:08:02 +0530

    added using generate button to get new quests from backend api

--- public/main.js
+++ public/main.js
@@ -2,11 +2,23 @@
 
 const generateBtn = document.getElementById('generate-btn')
 
+
 // console.log(generateBtn)
 // console.log(quest)
 
-const response = await fetch("/api/llm");
+// const response = await fetch("/api/llm");
+// const quest = await response.json();
 
-const quest = await response.json();
+// console.log(quest);
 
-console.log(quest);
+generateBtn.addEventListener('click', () => {
+    showQuest()
+})
+
+async function showQuest () {
+    const response = await fetch("/api/llm");
+    const quest = await response.json();
+
+    console.log(quest);
+
+}
