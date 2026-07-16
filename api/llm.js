2d6e4e5cbdce2012522b4ae9bb45b31b5b57b519
strawberryskeleton <xradhi5685@gmail.com>
2026-07-16 19:16:23 +0530

    setup getting quest in json format in backend from api

--- api/llm.js
+++ api/llm.js
@@ -1,5 +1,6 @@
-import { OpenAI } from "openai/client.js";
+import { OpenAI } from "openai";
 import dotenv from "dotenv"
+import { json } from "node:stream/consumers";
 dotenv.config()
 
 // console.log(process.env.GROQ_API_KEY);
@@ -10,3 +11,40 @@ const client = new OpenAI({
 });
 
 // console.log(client)
+
+const completion = await client.chat.completions.create({
+    model: "qwen/qwen3-32b",
+    temperature: 1.2,
+    messages: [
+        {
+            role: "system",
+            content: `You are a generator of daily quests.
+
+            Return ONLY valid JSON.
+
+            {
+            "task": "",
+            "difficulty": 1-5,
+            "category": "",
+            "estimatedMinutes": 5-60,
+            "xp": 25-150,
+            }
+
+            Rules:
+            - Safe
+            - Family friendly
+            - Creative
+            - Complete in one day
+            - Do not repeat common quests too often`
+        },
+        {
+            role: "user",
+            content: "Generate one daily quest."
+        }
+    ]
+})
+
+const content = completion.choices[0].message.content
+const quest = JSON.parse(content)
+
+console.log(quest)
